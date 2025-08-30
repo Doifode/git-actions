8d9721c22e399abd6bbbcb8574f7b47dc81ba08e
yashdevpl <yash.doifode@pi-labs.ai>
2025-08-30 18:25:35 +0530

    chore : basic cmds and practice done

---
+++ linux-unix/1-cmds/basics.js
@@ -0,0 +1,49 @@
+// yt_link ->  https://www.youtube.com/watch?v=e01GGTKmtpc
+/*
+date  -> show current date
+ls    -> list all files and folders under folder/directory
+ls -l -> gives detail view of files and folder of the folder where you run the cmd.
+pwd[present-working-directoru]   -> current position of you means where are you in folder structure of linux
+mkdir {folder-name} -> create folder / directory
+rmdir {folder-name} -> remove / delete folder with matching name
+touch {file.name}   -> create new file with given file with extension
+clear -> clear terminal history
+cd -> change directory / navigate inside any directory present in current directory.
+cd .. -> come out form directory
+rm {file-name} -> remove / delete file 
+rm -r-[recursive] -> remove directory / folder 
+cat file-name -> see the content inside the file
+echo "message-text" -> consoles or prints the message passed in the strings
+zcat -> to see the content inside the ziped folder or file
+head {file.name} -> see the top lines of the file [5]
+tail {file.name} -> show the last lines of file [5]
+tail -f -> watches on the file and logs the last logged lines / monitors file
+less {file.name} -> shows the file content in paginated way
+more {file.name} -> shows the file content in more paginated way
+
+/--------------------INTERMEDIRISE-------------------------
+
+cp {file.name.path} {new.path.to.copy} -> to copy file from one dir to another dir 
+cp -r [file.path.to.copy] [where.to.copy] -> -r copy the folder inside another folder
+
+mv {file.name.path} {new.path.to.move} -> to move file from one dir to another dir 
+mv -r [file.path.to.move] [where.to.move] -> -r move the folder inside another folder
+mv [file/folder.name] [new/file/folder.name] -> rename the folder or file 
+
+wc {file.name} -> gives the word count in file with |line | word | size of file || can check with multiple files
+
+cut -b 1-5 {file.name} -> returns the portion or cuts the portion from file in bytes
+
+ln -s <target> <link_name>  -> creates the shortcut of the given file name  | will delete the link if the file deletes
+ln <target> <link_name> -> cretates hard link of the given file | persists the file even the file deletes from actual space
+
+diff <file.name> <target.file.name> -> shows the difference between the two files only check two file difference
+
+
+
+
+
+
+
+
+*/
